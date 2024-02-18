19d1e230c9fa8dbc3eaa71e25ba8ee6744aa0547
makniker <ermakov.nd@edu.spbstu.ru>
2024-02-18 19:34:38 +0300

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,6 +3,8 @@ package ru.easycode.zerotoheroandroidtdd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 
@@ -13,10 +15,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id)
-        viewModel.load()
+        val button = findViewById<Button>(R.id.actionButton)
+        val text = findViewById<TextView>(R.id.titleTextView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        button.setOnClickListener { viewModel.load() }
         viewModel.liveDataWrapper.liveData().observe(
             this
-        ) { value -> value.apply() }
+        ) { value -> value.apply(text, button, progressBar) }
     }
 }
