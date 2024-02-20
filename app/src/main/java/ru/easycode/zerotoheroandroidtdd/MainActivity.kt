dc4546bda9e9cda02e72e561885d4703439f9466
makniker <ermakov.nd@edu.spbstu.ru>
2024-02-20 12:43:15 +0300

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,35 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.actionButton)
+        val text = findViewById<TextView>(R.id.titleTextView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        viewModel = (application as MyApp).viewModel
+        button.setOnClickListener { viewModel.load() }
+        viewModel.liveDataWrapper.liveData().observe(
+            this
+        ) { value ->
+            value.apply(text, button, progressBar) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val b = BundleWrapper.Mutable.Base(outState)
+        viewModel.save(b)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.restore(BundleWrapper.Mutable.Base(savedInstanceState))
     }
 }
